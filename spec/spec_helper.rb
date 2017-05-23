b44b0742d330d79a950a41d40ef058d3760f0578
Torsten Ruger <torsten@villataika.fi>
2017-05-23 18:19:37 +0300

    change to new codeclimate system

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,6 @@
 if ENV['CODECLIMATE_REPO_TOKEN']
-  require "codeclimate-test-reporter"
-  CodeClimate::TestReporter.start
+  require 'simplecov'
+  SimpleCov.start { add_filter "/spec/" }
 end
 
 # This file is copied to spec/ when you run 'rails generate rspec:install'
@@ -14,14 +14,14 @@ Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |f| require f }
 
 
 RSpec.configure do |config|
-  
-  config.include PageHelper  
-  
+
+  config.include PageHelper
+
   config.include OfficeClerk::Engine.routes.url_helpers
 
   config.infer_base_class_for_anonymous_controllers = false
   config.infer_spec_type_from_file_location!
-  
+
   # Run specs in random order to surface order dependencies. If you find an
   # order dependency and want to debug it, you can fix the order by providing
   # the seed, which is printed after each run.
@@ -32,7 +32,7 @@ RSpec.configure do |config|
     config.default_formatter = 'doc'
   end
 
-  # Print the 10 slowest examples and example groups at the end of the spec run, 
+  # Print the 10 slowest examples and example groups at the end of the spec run,
   #to help surface which specs are running  particularly slow.
 #  config.profile_examples = 10
 end
